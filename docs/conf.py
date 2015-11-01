b063c48feca07a87f264e2a1b984906aafc4056f
abu-wafiy <atoz.chevara@yahoo.com>
2015-11-01 23:29:47 +0700

    - Melakukan beberapa perbaikan pada berkas conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,9 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Terjemahan Shahih Muslim'
-copyright = u'2015, Aceh Creative Labs'
+copyright = u'2015, Abu Wafiy Al Atsyi'
+author = u'Abu Wafiy Al Usmani Al Atsyi'
+publisher = u'Aceh Creative Labs'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -176,7 +178,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'ReadtheDocsTemplatedoc'
+htmlhelp_basename = 'TerjemahanShahihMuslim'
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -196,8 +198,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'ReadtheDocsTemplate.tex', u'Read the Docs Template Documentation',
-   u'Read the Docs', 'manual'),
+  ('index', 'TerjemahanShahihMuslim.tex', u'Terjemahan Shahih Muslim',
+   u'Aceh Creative Labs', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -226,8 +228,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'readthedocstemplate', u'Read the Docs Template Documentation',
-     [u'Read the Docs'], 1)
+    ('index', 'terjemahanshahihmuslim', u'Terjemahan Shahih Muslim',
+     [u'Aceh Creative Labs'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -240,8 +242,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'ReadtheDocsTemplate', u'Read the Docs Template Documentation',
-   u'Read the Docs', 'ReadtheDocsTemplate', 'One line description of project.',
+  ('index', 'TerjemahanShahihMuslim', u'Terjemahan Shahih Muslim',
+   u'Aceh Creative Labs', 'TerjemahanShahihMuslim', 'One line description of project.',
    'Miscellaneous'),
 ]
 
